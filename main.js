a4c682513f70765779e99cb96fc9d2d5eec1def9
furvietto <fulvio-94@hotmail.it>
2021-11-22 16:29:02 +0100

    problem style

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const icon = document.querySelector(".fas.fa-bars");
+const icon = document.querySelector(".header-right a i");
 const hamburger = document.querySelector(".hamburger-menu"); 
-const iconx = document.querySelector(".fas.fa-times");
+const iconx = document.querySelector(".hamburger-menu a i");
+const prova = document.getElementById("red");
 
 icon.addEventListener('click', function() {
-    hamburger.classList.toggle("active");   
+    hamburger.classList.toggle("active"); 
+    prova.classList.add("red"); 
+    hamburger.style.background-color = "red"; 
 });
 
 iconx.addEventListener('click', function() {
