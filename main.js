4e2e3e8f696025f0141c55397d940672b15a5128
furvietto <fulvio-94@hotmail.it>
2021-11-22 14:34:34 +0100

    work done

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const icon = document.querySelector(".fas.fa-bars");
 const hamburger = document.querySelector(".hamburger-menu"); 
+const iconx = document.querySelector(".fas.fa-times");
 
 icon.addEventListener('click', function() {
-    hamburger.classList.add(".active");   
-})
+    hamburger.classList.add("active");   
+});
+
+iconx.addEventListener('click', function() {
+    hamburger.classList.remove("active");
+});
